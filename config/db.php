b8d0e325ef0e130633971fe6d39772abb969a4cd
Vasilii Chugunov <naukomania@gmail.com>
2016-12-29 02:29:16 +0300

    set db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-ang', // Хост и название базы данных
+    'username' => 'root', // Имя пользователя mysql
+    'password' => '123', // Пароль пользователя mysql
     'charset' => 'utf8',
 ];
